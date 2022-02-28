e56144c378a13da9a21dfe7e6f704d558dbfee3c
Patrick Dahms <pat.dahms@googlemail.com>
2022-02-28 16:27:40 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <meta content="_app.js" />
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+          <meta content="_document.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
